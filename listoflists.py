83d95bd0fd7db661c1ef59de4299ee8ea5c0a8b0
DuncanMathai <duncanmathai3@gmail.com>
2023-01-31 22:45:59 +0300

    lists inside a list

---
+++ listoflists.py
@@ -0,0 +1,16 @@
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+#house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+print(house)
+
+print(type(house))
