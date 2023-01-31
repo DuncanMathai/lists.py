34832aafad4a51f438974ad9e7da640e90c45c0e
DuncanMathai <duncanmathai3@gmail.com>
2023-01-31 22:31:37 +0300

    create lists

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a file created to show the areas of a house.
---
+++ index.py
@@ -0,0 +1,9 @@
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+areas = ["hallway", 11.25, "kitchen", 18.0, "livingroom", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+print(areas)
